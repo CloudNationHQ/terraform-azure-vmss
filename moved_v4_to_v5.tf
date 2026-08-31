e0a033f6c8a006e785b086ad0bda230c08b888fa
Jasper Post <jasper.post@cloudnation.nl>
2026-08-31 13:52:35 +0200

    auto-up

--- moved_v4_to_v5.tf
+++ moved_v4_to_v5.tf
@@ -1,8 +1,3 @@
-# v4 of this module renames the primary for_each key from "vmss" to "this"
-# on each scale-set type and the autoscale setting, aligning with the
-# universal "this" convention. These moved blocks migrate existing state
-# addresses so consumers upgrade without a destroy/recreate.
-
 moved {
   from = azurerm_linux_virtual_machine_scale_set.this["vmss"]
   to   = azurerm_linux_virtual_machine_scale_set.this["this"]
